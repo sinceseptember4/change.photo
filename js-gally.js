d49029de941f8f0f11c57d66533d9f3b5f0b2505
sinceseptember4 <sinceseptember4@gmail.com>
2021-07-08 13:40:47 +0900

    7/8

--- js-gally.js
+++ js-gally.js
@@ -2,17 +2,18 @@ var cnt = document.getElementById("list").childElementCount;
 console.log(cnt); 
 const list = document.querySelector('#list')
 if (cnt === 1)  {
-   button.classList.add('count1');
+   list.classList.add('count1');
  } else if (cnt == 2)  {
-   button.classList.add('count2');
+   list.classList.add('count2');
  }else if (cnt == 3)  {
    list.classList.add('count3');
  }else if (cnt == 4)  {
    list.classList.add("count4");
  }else if (cnt == 5)  {
    list.classList.add("count5");
- }else if (cnt == 6)  {
-   list.classList.add("count6");
  }else {
-   console.log("nodate");
+   var minus = cnt-4;
+   console.log(minus)
+   document.querySelector('.plus'),innerHTML.`<span>${minus}</span>`;
+   list.classList.add("count6");
  }
