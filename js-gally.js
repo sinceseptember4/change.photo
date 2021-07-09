cfca28a55e7f0a63fb940330327d67d7ced16585
sinceseptember4 <sinceseptember4@gmail.com>
2021-07-09 18:24:35 +0900

    7/10

--- js-gally.js
+++ js-gally.js
@@ -1,15 +1,31 @@
-var parent = document.querySelector('.js-gallery');
-console.log(parent);
-var cnt = $(".js-gallery li").length;
 
-console.log(cnt);
+var parent = document.querySelector('.js-gallery');
+var cnt = document.querySelectorAll('li').length;
+var img = document.querySelectorAll('img');
+
+var width =img[0].width;
+var height =img[0].height;
+var aspect1 = width / height;
+console.log(aspect1);
+
+var width3 =img[1].width;
+console.log(width3)
+var height3 =img[2].height;
+console.log(height3)
+var aspect3 = width3 / height3;
+console.log(aspect3); 
+
 if (cnt == 1)  {
    parent.classList.add('count1');
- } else if (cnt == 2)  {
-   parent.classList.add('count2');
- }else if (cnt == 3)  {
-   parent.classList.add('count3');
- }else if (cnt == 4)  {
+ } else if (cnt == 2 && aspect1 < 1)  {
+   parent.classList.add('count2-2');
+ }else if (cnt == 2 && aspect1 > 1)  {
+  parent.classList.add('count2');
+}else if (cnt == 3 )  {
+  parent.classList.add('count3');
+}else if (cnt == 3)  {
+  parent.classList.add('count3');
+}else if (cnt == 4)  {
    parent.classList.add("count4");
  }else if (cnt == 5)  {
    parent.classList.add("count5");
@@ -17,7 +33,9 @@ if (cnt == 1)  {
    var minus = cnt-4;
    console.log(minus);
    parent.classList.add("count6");
-   var textbox_element = document.querySelector('#list');
-   var new_element = innerHTML = `<h1>+${minus}</h1>`;
-   textbox_element.before(new_element);
+   var textbox_element = $(".js-gallery a");
+   var new_element =`<h1>+${minus}</h1>`;
+   console.log(new_element );
+   textbox_element.after(new_element);
  }
+ 
