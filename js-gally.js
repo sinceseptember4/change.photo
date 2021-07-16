cbb3c99ac034866993c0e1e474e7fd7b8b1dc5f0
sinceseptember4 <sinceseptember4@gmail.com>
2021-07-16 17:40:13 +0900

    7/16

--- js-gally.js
+++ js-gally.js
@@ -1,19 +1,19 @@
+const parent = document.querySelector('.js-gallery');
+const cnt = document.querySelectorAll('li').length;
+const img = document.querySelectorAll('img');
 
-var parent = document.querySelector('.js-gallery');
-var cnt = document.querySelectorAll('li').length;
-var img = document.querySelectorAll('img');
-
-var width =img[0].width;
-var height =img[0].height;
-var aspect1 = width / height;
+let width =img[0].width;
+let height =img[0].height;
+let aspect1 = width / height;
 console.log(aspect1);
 
-var width3 =img[1].width;
+let width3 =img[1].width;
 console.log(width3)
-var height3 =img[2].height;
+let height3 =img[1].height;
 console.log(height3)
-var aspect3 = width3 / height3;
+let aspect3 = width3 / height3;
 console.log(aspect3); 
+
 
 if (cnt == 1)  {
    parent.classList.add('count1');
@@ -21,11 +21,11 @@ if (cnt == 1)  {
    parent.classList.add('count2-2');
  }else if (cnt == 2 && aspect1 > 1)  {
   parent.classList.add('count2');
-}else if (cnt == 3 )  {
+ }else if (cnt == 3 )  {
   parent.classList.add('count3');
-}else if (cnt == 3)  {
+ }else if (cnt == 3)  {
   parent.classList.add('count3');
-}else if (cnt == 4)  {
+ }else if (cnt == 4)  {
    parent.classList.add("count4");
  }else if (cnt == 5)  {
    parent.classList.add("count5");
@@ -37,5 +37,4 @@ if (cnt == 1)  {
    var new_element =`<h1>+${minus}</h1>`;
    console.log(new_element );
    textbox_element.after(new_element);
- }
- 
+}
