6fe4e086f837876a9dc3fd9add199292a6809d60
sinceseptember4 <sinceseptember4@gmail.com>
2021-10-20 11:38:10 +0900

    aaa

--- change-photo.js
+++ change-photo.js
@@ -1,54 +1,5 @@
 "use strict";
 const gallery = document.querySelectorAll('.change-photo');
-<<<<<<< HEAD
-
-gallery.forEach(function(v){
-  let ul = v.querySelector('ul');
-  let cnt = ul.childElementCount;
-  let img = v.querySelector('img');
-
-  let width =img.width;
-  let height =img.height;
-  let aspect1 = width / height;
-<<<<<<< HEAD:js-gally.js
-=======
-  console.log(width);
-  console.log(height);
-  console.log(aspect1);
->>>>>>> 058250df0a8e98704290413bb18b490cdafdc8f6:change-photo.js
-
-if (height>!0.000000001 && width>!0.000000001){
-}else{  location.reload();
-}
-if (cnt == 1)  {
-   v.classList.add('count1');
- } else if (cnt == 2 && aspect1 < 1)  {
-   v.classList.add('count2-2');
- }else if (cnt == 2)  {
-  v.classList.add('count2');
- }else if (cnt == 3 && aspect1 > 1)  {
-  v.classList.add('count3-2');
- }else if (cnt == 3)  {
-  v.classList.add('count3');
- }else if (cnt == 4)  {
-   v.classList.add("count4");
- }else if (cnt == 5)  {
-   v.classList.add("count5");
- }else {
-   var minus = cnt-4;
-   console.log(minus);
-   v.classList.add("count6");
-   var photo4 = ul.children[4];
-   photo4.insertAdjacentHTML('beforeend', `<h1>+${minus}</h1>`);
-
-   var children4 = ul.children[4];
-   var clientHeight = children4.clientHeight;
-   var countfontsize = `${clientHeight*0.1}px`;
-   children4.style.fontSize = countfontsize;
-   children4.insertAdjacentHTML('beforeend', `<h1>+${minus}</h1>`);
-}
-});
-
 gallery.forEach(function (v) {
     let ul = v.querySelector('ul');
     let cnt = ul.childElementCount;
